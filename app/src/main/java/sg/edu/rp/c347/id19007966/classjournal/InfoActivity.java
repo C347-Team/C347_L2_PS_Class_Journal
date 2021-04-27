3acf09760d1954bb62152d46f7e2f5b7c8c01799
19024292 <19024292@myrp.edu.sg>
2021-04-27 14:43:25 +0800

    buttons

--- app/src/main/java/sg/edu/rp/c347/id19007966/classjournal/InfoActivity.java
+++ app/src/main/java/sg/edu/rp/c347/id19007966/classjournal/InfoActivity.java
@@ -2,24 +2,34 @@ package sg.edu.rp.c347.id19007966.classjournal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class InfoActivity extends AppCompatActivity {
-
     ListView listView;
     ArrayAdapter adapter;
     ArrayList<Week> weeks;
 
+    Button btnInfo;
+    Button btnAdd;
+    Button btnEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
         listView = findViewById(R.id.infoList);
+
+        btnInfo = findViewById(R.id.buttonInfo);
+        btnAdd = findViewById(R.id.buttonAdd);
+        btnEmail = findViewById(R.id.buttonEmail);
 
         weeks = new ArrayList<>();
         // week 1
@@ -31,5 +41,33 @@ public class InfoActivity extends AppCompatActivity {
 
         listView.setAdapter(adapter);
 
+        btnInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // Intent to display data
+                Intent rpIntent = new Intent(Intent.ACTION_VIEW);
+                // Set the URL to be used.
+                rpIntent.setData(Uri.parse("https://www.rp.edu.sg/soi/full-time-diplomas/details/diploma-in-digital-design-and-development"));
+                startActivity(rpIntent);
+            }
+        });
+
+        btnEmail.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                // The action you want this intent to do;
+                // ACTION_SEND is used to indicate sending text
+                Intent email = new Intent(Intent.ACTION_SEND);
+                // Put essentials like email address, subject & body text
+                email.putExtra(Intent.EXTRA_EMAIL,
+                        new String[]{"jason_lim@rp.edu.sg"});
+                // This MIME type indicates email
+                email.setType("message/rfc822");
+                // createChooser shows user a list of app that can handle
+                // this MIME type, which is, email
+                startActivity(Intent.createChooser(email,
+                        "Choose an Email client :"));
+
+            }});
     }
 }
