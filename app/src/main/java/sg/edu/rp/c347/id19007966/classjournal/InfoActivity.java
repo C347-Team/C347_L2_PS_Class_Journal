8461f665bc9c72432a0999e276082aef1538b442
Vincent <23420208+vincentneo@users.noreply.github.com>
2021-04-27 12:46:04 +0800

    preliminary additions

--- app/src/main/java/sg/edu/rp/c347/id19007966/classjournal/InfoActivity.java
+++ app/src/main/java/sg/edu/rp/c347/id19007966/classjournal/InfoActivity.java
@@ -2,15 +2,13 @@ package sg.edu.rp.c347.id19007966.classjournal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class InfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        startActivity(new Intent(MainActivity.this, InfoActivity.class));
+        setContentView(R.layout.activity_info);
     }
 }
